abdf43bb82ed6fcac66eedaeb490dc95ab601a03
Nathael Bonnal <kirua48000@gmail.com>
2021-03-22 09:48:37 +0100

    Auto stash before merge of "master" and "origin/master"

--- IHM.py
+++ IHM.py
@@ -1,4 +1,7 @@
 from tkinter import Tk, ttk, Menu
+from tkinter import *
+from tkinter import ttk
+import tkinter as tk
 from tkinter.ttk import Notebook
 
 from datagrid import Datagrid
@@ -9,13 +12,20 @@ class Fenetre(Tk):
         self.nombre:int = 10
         self.title("Médiathèque")
         self.geometry('640x480')
+        self.onglets = ttk.Notebook(self)  # Création du système d'onglets
+        self.onglets.pack(fill=tk.X)
+        o1 = ttk.Frame(self.onglets)  # Ajout de l'onglet 1
+        o1.pack(side=LEFT)
+        o2 = ttk.Frame(self.onglets)  # Ajout de l'onglet 2
+        o2.pack(side=LEFT)
+        self.onglets.add(o1, text='Onglet 1')  # Nom de l'onglet 1
+        self.onglets.add(o2, text='Onglet 2')  # Nom de l'onglet 2
 
 class Medias(ttk.Notebook):
     def __init__(self,fenetre):
         self.fenetre = fenetre
         self.pack()
         self.add(Creerlivre(self),text="creer livre")
-
 
 
 class Creerlivre(ttk.Frame):
@@ -27,6 +37,18 @@ class Creerlivre(ttk.Frame):
         l2.pack(padx = 5, pady = 5)
         b1 = ttk.Button()
 
+
+        
+
+
+
+        # data_csv = "colonne1:50;colonne2:100;colonne3:50\n11;12;13\n21;22;23"
+        # self.data = Datagrid(self, data_csv)
+        # self.data.pack()
+
+class Medias(Frame):
+    def __init__(self):
+        super().__init__()
 
 
 

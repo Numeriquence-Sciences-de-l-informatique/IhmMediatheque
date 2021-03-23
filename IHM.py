7320fac510500f2ee1112915dbe21dbc84acb7cd
Nathael Bonnal <kirua48000@gmail.com>
2021-03-23 10:22:49 +0100

    [🐠] Onglets was created

--- IHM.py
+++ IHM.py
@@ -5,6 +5,7 @@ import tkinter as tk
 from tkinter.ttk import Notebook
 
 from datagrid import Datagrid
+
 class Fenetre(Tk):
     def __init__(self):
         super().__init__()
@@ -13,13 +14,17 @@ class Fenetre(Tk):
         self.title("Médiathèque")
         self.geometry('640x480')
         self.onglets = ttk.Notebook(self)  # Création du système d'onglets
-        self.onglets.pack(fill=tk.X)
+        self.onglets.pack(fill=tk.BOTH, expand=1)
         o1 = ttk.Frame(self.onglets)  # Ajout de l'onglet 1
         o1.pack(side=LEFT)
         o2 = ttk.Frame(self.onglets)  # Ajout de l'onglet 2
         o2.pack(side=LEFT)
         self.onglets.add(o1, text='Onglet 1')  # Nom de l'onglet 1
         self.onglets.add(o2, text='Onglet 2')  # Nom de l'onglet 2
+        data_csv = "colonne1:50;colone2:100;colonne3:50\n11;12;13\n21;22;23"
+        data = Datagrid(o1, data_csv)
+        data.pack()
+
 
 class Medias(ttk.Notebook):
     def __init__(self,fenetre):

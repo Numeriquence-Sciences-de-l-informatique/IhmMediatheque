078de59ca4472b35f560a49df4b2849e13ba70da
Nathael Bonnal <kirua48000@gmail.com>
2021-03-23 10:53:29 +0100

    [🚀] Button was Created in Médiathèque Notebook

--- IHM.py
+++ IHM.py
@@ -1,7 +1,11 @@
 from tkinter import Tk, ttk, Menu
+from tkinter import *
+from tkinter import ttk
+import tkinter as tk
 from tkinter.ttk import Notebook
 
 from datagrid import Datagrid
+
 class Fenetre(Tk):
     def __init__(self):
         super().__init__()
@@ -9,13 +13,35 @@ class Fenetre(Tk):
         self.nombre:int = 10
         self.title("Médiathèque")
         self.geometry('640x480')
+        self.onglets = ttk.Notebook(self)  # Création du système d'onglets
+        self.onglets.pack(fill=tk.BOTH, expand=1)
+        o1 = ttk.Frame(self.onglets)  # Ajout de l'onglet 1
+        o1.pack(side=LEFT)
+        o2 = ttk.Frame(self.onglets)  # Ajout de l'onglet 2
+        o2.pack(side=LEFT)
+        self.onglets.add(o1, text='Médiathèque')  # Nom de l'onglet 1
+        self.onglets.add(o2, text='Adhérents')  # Nom de l'onglet 2
+
+        """
+        Sélection du document 
+        """
+        rad1 = Radiobutton(o1, text='Tout les Documents', value=1)
+        rad2 = Radiobutton(o1, text='Livres', value=2)
+        rad3 = Radiobutton(o1, text='CD', value=3)
+
+        rad1.grid(column=0, row=0)
+        rad2.grid(column=1, row=0)
+        rad3.grid(column=2, row=0)
+
+        lbl = Label(o2, text="Hello", font=("Arial Bold", 50))
+        lbl.pack()
+
 
 class Medias(ttk.Notebook):
     def __init__(self,fenetre):
         self.fenetre = fenetre
         self.pack()
         self.add(Creerlivre(self),text="creer livre")
-
 
 
 class Creerlivre(ttk.Frame):
@@ -26,8 +52,7 @@ class Creerlivre(ttk.Frame):
         l2 = ttk.Label(text="Auteur")
         l2.pack(padx = 5, pady = 5)
         b1 = ttk.Button()
-
-
+        b1.pack()
 
 
 def main():

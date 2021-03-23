7d6232ddafdf37afbc5eaa77c8c290010dd0d653
Pierre Lemaitre <oultetman@sfr.fr>
2021-03-23 21:26:38 +0100

    Modification Medias ajout super ajout creer Cd dans les onglets

--- IHM.py
+++ IHM.py
@@ -15,7 +15,7 @@ class Fenetre(Tk):
         self.geometry('640x480')
         self.onglets = ttk.Notebook(self)  # Création du système d'onglets
         self.onglets.pack(fill=tk.BOTH, expand=1)
-        o1 = ttk.Frame(self.onglets)  # Ajout de l'onglet 1
+        o1 = Medias(self.onglets)  # Ajout de l'onglet 1
         o1.pack(side=LEFT)
         o2 = ttk.Frame(self.onglets)  # Ajout de l'onglet 2
         o2.pack(side=LEFT)
@@ -33,15 +33,17 @@ class Fenetre(Tk):
         # rad2.grid(column=1, row=0)
         # rad3.grid(column=2, row=0)
         #
-        # lbl = Label(o2, text="Hello", font=("Arial Bold", 50))
-        # lbl.pack()
+        lbl = Label(o2, text="Hello", font=("Arial Bold", 50))
+        lbl.pack()
 
 
 class Medias(ttk.Notebook):
     def __init__(self,fenetre):
+        super().__init__(fenetre)
         self.fenetre = fenetre
         self.pack()
         self.add(Creerlivre(self),text="creer livre")
+        self.add(CreerCD(self), text="creer CD")
 
 
 class Creerlivre(ttk.Frame):

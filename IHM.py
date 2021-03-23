ec6e5c42be520432d3d6d6a5b93bcf33c49fa8f1
Nathael Bonnal <kirua48000@gmail.com>
2021-03-23 12:08:51 +0100

    [🚀] Merging

--- IHM.py
+++ IHM.py
@@ -1,16 +1,14 @@
-from tkinter import Tk, ttk, Menu
+from tkinter.ttk import Notebook, Progressbar
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
-from tkinter.ttk import Notebook
 
-from datagrid import Datagrid
 
 class Fenetre(Tk):
     def __init__(self):
         super().__init__()
-        self.texte:str = "Hi"
-        self.nombre:int = 10
+        self.texte: str = "Hi"
+        self.nombre: int = 10
         self.title("Médiathèque")
         self.geometry('640x480')
         self.onglets = ttk.Notebook(self)  # Création du système d'onglets
@@ -36,29 +34,55 @@ class Fenetre(Tk):
         lbl = Label(o2, text="Hello", font=("Arial Bold", 50))
         lbl.pack()
 
+        style = ttk.Style()
+
+        style.theme_use('classic')
+
+        style.configure("black.Horizontal.TProgressbar", background='black')
+
+        bar = Progressbar(o2, length=200, style='black.Horizontal.TProgressbar')
+
 
 class Medias(ttk.Notebook):
-    def __init__(self,fenetre):
+    def __init__(self, fenetre):
         self.fenetre = fenetre
         self.pack()
-        self.add(Creerlivre(self),text="creer livre")
+        self.add(Creerlivre(self), text="creer livre")
 
 
 class Creerlivre(ttk.Frame):
-    def __init__(self,parent):
+    def __init__(self, parent):
+        super().__init__(parent)
         self.parent = parent
         l1 = ttk.Label(text="titre")
-        l1.pack(padx = 5, pady = 5)
+        l1.pack(padx=5, pady=5)
         l2 = ttk.Label(text="Auteur")
-        l2.pack(padx = 5, pady = 5)
+        l2.pack(padx=5, pady=5)
         b1 = ttk.Button()
         b1.pack()
+        self.onglets.pack(fill=tk.X)
+        o1 = ttk.Frame(self.onglets)  # Ajout de l'onglet 1
+        o1.pack(side=LEFT)
+        o2 = ttk.Frame(self.onglets)  # Ajout de l'onglet 2
+        o2.pack(side=LEFT)
+        self.onglets.add(o1, text='Onglet 1')  # Nom de l'onglet 1
+        self.onglets.add(o2, text='Onglet 2')  # Nom de l'onglet 2
+
+        # data_csv = "colonne1:50;colonne2:100;colonne3:50\n11;12;13\n21;22;23"
+        # self.data = Datagrid(self, data_csv)
+        # self.data.pack()
+
+
+class Medias(Frame):
+    def __init__(self):
+        super().__init__()
 
 
 def main():
     f = Fenetre()
 
     f.mainloop()
+
 
 if __name__ == '__main__':
     main()
